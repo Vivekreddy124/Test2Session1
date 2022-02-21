1269d7dce77a16cbe97813a3f1f2335d668cb432
Vivekreddy124 <95470129+Vivekreddy124@users.noreply.github.com>
2022-02-21 06:46:36 +0000

    not done yet

--- p1original.c
+++ p1original.c
@@ -1,2 +1,19 @@
-
+#inclucde<stdio.h>
+void input(float *x1,float *y1,float *y2,float *y2);
+{
+  printf("enter the two numbers to find the distance between them");
+  scanf("%f%f%f" ,x1,y1,x2,y2);
+}
+void find_distance(float x1,float y2,float x1,float y2,float *s);
+{
+  *s= (sqrt(x2-x1)*(x2-x1)+(y2-y1)*(y2-y1);
+}
+void output (float x1,float y1,float x2,float y2,float s);
+{
+  printf("the distance between thyem is (%f%f) and (%f%f) is %f\n",x1,y1,x2,y2,s);
+}
+void main ()
+{
+  int x1,y1,x2,y2,s;
+  input ()
 }
